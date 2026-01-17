8094d7826d1dc9ded1fd4d54761ae60f832466d1
Сайсанов Дмитрий Алексеевич <saysanov2018@mail.ru>
2026-01-17 19:39:53 +0500

    feat: добавлены кнопки и немного адаптива

--- src/components/ui/UserItem.vue
+++ src/components/ui/UserItem.vue
@@ -8,14 +8,20 @@ const { user } = defineProps<{
 
 <template>
     <div class="user-card">
-        <div class="user-photo">
-            <img :src="user.image" alt="Фото" />
-        </div>
-
         <div class="user-info">
-            <h3>{{ user.name }} {{ user.surname }}</h3>
-            <p>Возраст: {{ user.age }}</p>
-            <p>Email: {{ user.email }}</p>
+            <div class="user-photo">
+                <img :src="user.image" alt="Фото" />
+            </div>
+
+            <div class="user-fio">
+                <h3>{{ user.name }} {{ user.surname }}</h3>
+                <p>Возраст: {{ user.age }}</p>
+                <p>Email: {{ user.email }}</p>
+            </div>
+        </div>
+        <div class="button">
+            <ElButton>Загрузить фото</ElButton>
+            <ElButton>Удалить фото</ElButton>
         </div>
     </div>
 </template>
@@ -27,6 +33,13 @@ const { user } = defineProps<{
     padding: 12px;
     border: 1px solid #eee;
     border-radius: 8px;
+    justify-content: space-between;
+    overflow: hidden;
+}
+
+.user-info {
+    display: flex;
+    gap: 12px;
 }
 
 .user-photo img {
@@ -35,14 +48,38 @@ const { user } = defineProps<{
     border-radius: 20%;
 }
 
-.user-info h3 {
+.user-fio {
+    max-width: 100px;
+}
+
+.user-fio h3 {
     margin: 0;
     font-size: 16px;
 }
 
-.user-info p {
+.user-fio p {
     margin: 2px 0 0;
     font-size: 14px;
     color: #666;
 }
+
+.button {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+}
+
+@media (max-width: 600px) {
+  .user-fio {
+    max-width: 120px;
+    min-width: 0;
+  }
+
+  .user-fio p {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
+
 </style>
