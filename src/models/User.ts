2c8988cfff98129da86c01ac4747e158b53631b8
Сайсанов Дмитрий Алексеевич <saysanov2018@mail.ru>
2026-01-17 23:15:55 +0500

    feat: обновлено для деплоя

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,7 +4,7 @@ export interface User {
     surname: string
     age: number
     email: string
-    image: string | null
+    image?: string | null
 }
 
 export type UserUploadDelImage = Pick<User, 'id' | 'image'>
