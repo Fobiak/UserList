648beb429b20dfece68c85f165795f29aec35530
Сайсанов Дмитрий Алексеевич <saysanov2018@mail.ru>
2026-02-16 18:39:50 +0500

    refactor: обновлена верстка, добавлены новые иконки

--- src/components/ui/UserFilter.vue
+++ src/components/ui/UserFilter.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { SortByEnum, SortDirectionEnum, type SortDirectionType, type SortType } from '@/constants/filters';
-import { CaretBottom, CaretTop } from '@element-plus/icons-vue'
+import SortUpIcon from '@/shared/assets/icons/SortUpIcon.vue';
+import SortDownIcon from '@/shared/assets/icons/SortDownIcon.vue';
 
 const props = defineProps<{
     isAvailableSort: boolean
@@ -12,21 +13,22 @@ const sortBy = defineModel<SortType | null>('sortBy')
 const sortDirection = defineModel<SortDirectionType>('sortDirection')
 const ageCheckbox = defineModel<boolean>('onlyAdults')
 
-const caretIcon = computed(() => sortDirection.value === SortDirectionEnum.ASC ? CaretTop : CaretBottom)
+const caretIcon = computed(() => sortDirection.value === SortDirectionEnum.ASC ? SortUpIcon : SortDownIcon)
 </script>
 
 <template>
     <div class="block">
         <ElInput v-model="searchText" placeholder="Поиск" />
-        <ElSelect v-model="sortBy" placeholder="Сортировка" clearable>
-            <ElOption label="По имени" :value="SortByEnum.NAME" />
-            <ElOption label="По возрасту" :value="SortByEnum.AGE" />
-        </ElSelect>
-        <ElButton v-if="props.isAvailableSort" @click="props.toggleSort">
-            <ElIcon>
+        <div class="sort">
+            <ElSelect v-model="sortBy" placeholder="Сортировка" clearable class="select"
+                :class="{ 'select-full': !isAvailableSort }">
+                <ElOption label="По имени" :value="SortByEnum.NAME" />
+                <ElOption label="По возрасту" :value="SortByEnum.AGE" />
+            </ElSelect>
+            <ElButton v-if="isAvailableSort" class="sort-button" @click="props.toggleSort">
                 <component :is="caretIcon" />
-            </ElIcon>
-        </ElButton>
+            </ElButton>
+        </div>
         <ElCheckbox v-model="ageCheckbox">Больше 18 лет</ElCheckbox>
     </div>
 </template>
@@ -40,10 +42,55 @@ const caretIcon = computed(() => sortDirection.value === SortDirectionEnum.ASC ?
     width: 100%;
 }
 
+.block :first-child {
+    flex: 4;
+}
+
+.sort {
+    flex: 2;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.block :last-child {
+    flex: 1;
+}
+
+.select {
+    width: 100%;
+}
+
+.select :deep(.el-select__wrapper) {
+    border-right: none;
+    border-radius: 4px 0 0 4px;
+}
+
+.select-full :deep(.el-select__wrapper) {
+    border-right: 1px;
+    border-radius: 2px;
+}
+
+.sort-button {
+    width: 16px;
+    padding: 0;
+    border-left: none;
+    border-radius: 0 4px 4px 0;
+}
+
 @media (max-width: 600px) {
     .block {
         flex-direction: column;
         gap: 10px;
+    }
+
+    .sort {
+        width: 100%;
+        display: flex;
+    }
+
+    .select {
+        flex: 1;
     }
 }
 </style>
---
+++ src/shared/assets/icons/SortDownIcon.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M3 4H7"
+      stroke="currentColor"
+      stroke-opacity="0.4"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+    <path
+      d="M3 8H10"
+      stroke="currentColor"
+      stroke-opacity="0.4"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+    <path
+      d="M3 12H13"
+      stroke="currentColor"
+      stroke-opacity="0.4"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
